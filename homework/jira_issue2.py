d4cfa47b0061f131c91fd43a9bc0d0ffdef7ecba
mjoyce6500@outlook.com <mjoyce6500@outlook.com>
2019-01-21 19:37:00 -0500

    Updated something, commited with script

--- homework/jira_issue2.py
+++ homework/jira_issue2.py
@@ -5,7 +5,8 @@
 
 from jira import JIRA
 import datetime
-yest = datetime.datetime.strftime(datetime.datetime.now()-datetime.timedelta(3),'%Y-%m-%d')
+yest = datetime.datetime.strftime(datetime.datetime.now()-datetime.timedelta(12),'%Y-%m-%d')
+keep: int = '15'
 # using this site as a ref: https://jira.readthedocs.io/en/master/examples.html#searching
 # This connects to a JIRA started on your local machine at http://localhost:2990/jira, which not coincidentally is the default address for a JIRA instance started from the Atlassian Plugin SDK.
 # You can manually set the JIRA server to use:
@@ -16,12 +17,12 @@ jira = JIRA(options)
 # https://jira.atlassian.com/browse/
 projects = jira.projects()
 f = open("jira_output2.md","w")
-f.writelines("# JIRA JQL results \n Query last 10 issues created " + yest + '\n \n')
-# Summaries of 3 issues created in last 1 days
-f.writelines("| Key | Icon | Requestor | Summary | \n")
-f.writelines("| --- | --- | ---| --- | \n")
-for issue in jira.search_issues('createdDate <= ' + yest + ' order by created desc', maxResults=5):
-    f.writelines('| [{}](https://jira.atlassian.com/browse/{}) | ![icon]({} "{}") | _by_ **{}**' .format(issue.key, issue.key, issue.fields.issuetype.iconUrl, issue.fields.issuetype.name, issue.fields.reporter.displayName))
-    f.writelines('| {} |\n'.format(issue.fields.summary))
-    f.writelines(issue.raw['fields'])
+f.writelines("# JIRA JQL results \n Query last " + keep + " issues created " + yest + '\n \n')
+# Summaries of last 'keep' issues created in last 'yest' days
+f.writelines("| Key | Icon | Requester | Due Date | Status | Summary | \n")
+f.writelines("| --- | --- | ---| --- | --- | --- | \n")
+for issue in jira.search_issues('createdDate <= ' + yest + ' order by created desc', maxResults = keep):
+    f.writelines('| [{}](https://jira.atlassian.com/browse/{}) | ![icon]({} "{}") | **{}**' .format(issue.key, issue.key, issue.fields.issuetype.iconUrl, issue.fields.issuetype.name, issue.fields.reporter.displayName))
+    f.writelines('| {} | {} | {} |\n'.format(issue.fields.duedate, issue.fields.status, issue.fields.summary))
+
 f.close()
