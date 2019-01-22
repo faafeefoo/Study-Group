20e66c570b11eef0894f9eeaae39ca84294fc72c
mjoyce6500@outlook.com <mjoyce6500@outlook.com>
2019-01-21 19:49:44 -0500

    Updated something, commited with script

--- homework/jira_issue2.py
+++ homework/jira_issue2.py
@@ -5,8 +5,8 @@
 
 from jira import JIRA
 import datetime
-yest = datetime.datetime.strftime(datetime.datetime.now()-datetime.timedelta(22),'%Y-%m-%d')
-keep: int = '15'
+date1 = datetime.datetime.strftime(datetime.datetime.now()-datetime.timedelta(2),'%Y-%m-%d')
+keep: int = '10'
 # using this site as a ref: https://jira.readthedocs.io/en/master/examples.html#searching
 # This connects to a JIRA started on your local machine at http://localhost:2990/jira, which not coincidentally is the default address for a JIRA instance started from the Atlassian Plugin SDK.
 # You can manually set the JIRA server to use:
@@ -17,11 +17,11 @@ jira = JIRA(options)
 # https://jira.atlassian.com/browse/
 projects = jira.projects()
 f = open("jira_output2.md","w")
-f.writelines("# JIRA JQL results \n Query last " + keep + " issues created " + yest + '\n \n')
-# Summaries of last 'keep' issues created in last 'yest' days
+f.writelines("# JIRA JQL results \n Query last **"+keep+"** issues created <span style=\"color:blue\"> " + date1 + '</span> \n \n')
+# Summaries of last 'keep' issues created in last 'date1' days
 f.writelines("| Key | Icon | Requester | Due Date | Status | Summary | \n")
 f.writelines("| --- | --- | ---| --- | --- | --- | \n")
-for issue in jira.search_issues('createdDate <= ' + yest + ' order by created desc', maxResults = keep):
+for issue in jira.search_issues('createdDate <= ' + date1 + ' order by created desc', maxResults = keep):
     f.writelines('| [{}](https://jira.atlassian.com/browse/{}) | ![icon]({} "{}") | **{}**' .format(issue.key, issue.key, issue.fields.issuetype.iconUrl, issue.fields.issuetype.name, issue.fields.reporter.displayName))
     f.writelines('| {} | {} | {} |\n'.format(issue.fields.duedate, issue.fields.status, issue.fields.summary))
 
